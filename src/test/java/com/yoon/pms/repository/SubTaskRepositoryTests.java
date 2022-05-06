2935deb7fad269a7cf9fac35f0f7af79c251e9b3
윤주영 <jyyoon@ibleaders.co.kr>
2022-05-06 11:17:13 +0900

    + CASCADE 테스트 코드 작성

--- src/test/java/com/yoon/pms/repository/SubTaskRepositoryTests.java
+++ src/test/java/com/yoon/pms/repository/SubTaskRepositoryTests.java
@@ -1,7 +1,7 @@
 package com.yoon.pms.repository;
 
 
-import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.DisplayName; 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -13,7 +13,6 @@ import com.yoon.pms.aop.AspectAdvice;
 import lombok.extern.slf4j.Slf4j;
 
 @SpringBootTest
-@Slf4j
 @Import(AspectAdvice.class)
 @Transactional
 public class SubTaskRepositoryTests{
